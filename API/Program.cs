5d59c6a24f8c135367597ed17a2149e277cbcccb
Hasan TEKGÜL <htekgulds@gmail.com>
2022-11-18 22:18:08 +0300

    actuators & tracing

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Tracing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+# region Steeltoe
+builder.AddAllActuators();
+
+builder.Services.AddDistributedTracingAspNetCore();
+# endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
